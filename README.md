f2d073195d26417c99bf4b7bd138caf1e7475336
Cbragg-code <braggc1@etsu.edu>
2025-07-15 13:08:57 -0400

    fix .gitignore and clean tracked files.

--- README.md
+++
@@ -1,2 +0,0 @@
-# python-supermarket
-python version of my original data structures supermarket simulation project (C#)
--- supermarket_logic/item.py
+++
@@ -1,7 +0,0 @@
-class Item:
-    def __init__(self, name, price):
-        self.name = name
-        self.price = price
-
-    def __repr__(self):
-        return f"{self.name} (${self.price:.2f})"
